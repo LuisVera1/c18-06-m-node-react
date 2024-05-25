29e6d55104a5b2517ac75289ccb50d58c3d1f7ef
Paola Gonzalez <paolagonzalez.contacto@gmail.com>
2024-05-24 23:06:29 -0400

    login commit

--- src/app/login/components/login.tsx
+++ src/app/login/components/login.tsx
@@ -4,14 +4,15 @@ import React, { useState } from "react";
 import { InputText } from "primereact/inputtext";
 import { Button } from "primereact/button";
 import { useUser } from "../../../context/UserContext"; // Importa el contexto del usuario
-import { useRouter } from 'next/navigation'; // Usa next/navigation en lugar de next/router
+import { useRouter } from "next/navigation"; // Usa next/navigation en lugar de next/router
 import { NextPage } from "next"; // Importa NextPage en lugar de React.FC
-import Image from 'next/image';
-import BannerLogin from "../../../../assets/container_2.png";
+import Image from "next/image";
+import BannerLogin from "../../../../assets/login.png";
 
 const Login: NextPage = () => {
     const [email, setEmail] = useState<string>("");
     const [password, setPassword] = useState<string>("");
+    const [role, setRole] = useState<string>("alumno");
     const [errors, setErrors] = useState<{ email?: string; password?: string }>({});
     const { setUser } = useUser(); // Usa el contexto del usuario
     const router = useRouter(); // Usa el hook useRouter
@@ -45,10 +46,16 @@ const Login: NextPage = () => {
         setErrors(newErrors);
 
         if (Object.keys(newErrors).length === 0) {
-            console.log("Formulario válido, enviando datos...");
-            // Envío de datos al backend
+            const roleToUrlMap: { [key: string]: string } = {
+                alumno: `${process.env.URL_BASE}/api/student/login`,
+                docente: `${process.env.URL_BASE}/api/teacher/login`,
+                admin: `${process.env.URL_BASE}/api/admin/login`,
+            };
+
+            const loginUrl = roleToUrlMap[role];
+
             try {
-                const response = await fetch("http://localhost:3000/api/student/login", {
+                const response = await fetch(loginUrl, {
                     method: "POST",
                     headers: {
                         "Content-Type": "application/json",
@@ -60,10 +67,20 @@ const Login: NextPage = () => {
                 });
 
                 if (response.ok) {
-                    const user = await response.json();
-                    setUser(user); // Actualiza el contexto del usuario
-                    console.log("Datos enviados correctamente");
-                    router.push("/inicio"); // Redirige al inicio solo si fue exitoso
+                    const data = await response.json();
+                    if (data.success) {
+                        const roleToRouteMap: { [key: string]: string } = {
+                            alumno: "/bienvenida/alumno",
+                            docente: "/bienvenida/docente",
+                            admin: "/bienvenida/admin",
+                        };
+                        const route = roleToRouteMap[role] || "/";
+                        router.push(route); // Redirige al inicio solo si fue exitoso
+
+                        setUser(data); // Actualiza el contexto del usuario
+                    } else {
+                        setErrors({ email: "Credenciales incorrectas" });
+                    }
                 } else {
                     console.error("Error al enviar los datos al servidor");
                 }
@@ -77,7 +94,23 @@ const Login: NextPage = () => {
         <div className="flex h-screen">
             <div className="card w-1/2 flex items-center justify-center">
                 <form className="flex flex-col gap-3 py-5 w-full" onSubmit={handleSubmit}>
-                    <p className="text-primary font-bold text-3xl mb-10 text-center font-barlow">Bienvenid@</p>
+                    <p className="text-primary font-bold text-3xl mb-10 text-center font-barlow">Iniciar sesión</p>
+
+                    <div className="flex flex-col items-center gap-2 w-full">
+                        <div className="flex flex-col w-2/4 items-center">
+                            <label htmlFor="role" className="text-dark text-left font-barlow"></label>
+                            <select
+                                id="role"
+                                value={role}
+                                onChange={(e) => setRole(e.target.value)}
+                                className="w-40 border-2 border-primary rounded bg-action text-primary py-1 px-2 text-xs mb-1 font-medium font-sans text-center"
+                            >
+                                <option value="alumno">Alumno</option>
+                                <option value="docente">Docente</option>
+                                <option value="admin">Admin</option>
+                            </select>
+                        </div>
+                    </div>
 
                     <div className="flex flex-col items-center gap-2 w-full">
                         <div className="flex flex-col w-2/4">
@@ -121,13 +154,7 @@ const Login: NextPage = () => {
                 </form>
             </div>
             <div className="w-1/2">
-                <Image
-                    className="w-full h-full object-cover"
-                    src={BannerLogin}
-                    alt="img-login"
-                    objectFit="cover"
-                    quality={100}
-                />
+                <Image className="w-full h-full object-cover" src={BannerLogin} alt="img-login" quality={100} priority />
             </div>
         </div>
     );
