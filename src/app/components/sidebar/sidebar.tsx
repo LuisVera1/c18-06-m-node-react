3f8b6a2a496297cf3a4754e3a2a410cabadb52da
Eduardo Aguilar <Agu1laredu@hotmail.com>
2024-05-27 21:53:23 -0300

    home vistaaa

--- src/app/components/sidebar/sidebar.tsx
+++ src/app/components/sidebar/sidebar.tsx
@@ -7,12 +7,15 @@ import { BiRadar, BiShoppingBag } from "react-icons/bi";
 import { GiChart } from "react-icons/gi";
 import { TbReportSearch } from "react-icons/tb";
 import { BsClipboard2DataFill, BsChevronRight, BsChevronDoubleRight } from "react-icons/bs";
+import { IoSettings } from "react-icons/io5";
+import { CiCircleQuestion } from "react-icons/ci";
+import { IoIosLogOut } from "react-icons/io";
 
 export default function Sidebar() {
     const pathname = usePathname();
 
     return (
-        <div className='bg-grey h-screen p-4 w-80 shadow-md'>
+        <div className='bg-grey h-screen p-4 w-80 shadow-md flex flex-col justify-between'>
             <nav className="lg:flex-1 overflow-y-auto w-80 mt-20">
                 <b className="text-primary m-4">MatrículaXpress</b>
                 <ul className="p-2 space-y-2 mt-40 text-justify mr-4">
@@ -24,6 +27,11 @@ export default function Sidebar() {
                     <NavItem href="/gestionfacturacion" icon={<BsClipboard2DataFill className="size-5 m-4" />} text="Gestión de pagos y facturación" isActive={pathname === "/gestionfacturacion"} />
                 </ul>
             </nav>
+            <div className="flex justify-around text-primary mt-4">
+                <IoSettings className="text-2xl cursor-pointer" />
+                <CiCircleQuestion className="text-2xl cursor-pointer" />
+                <IoIosLogOut className="text-2xl cursor-pointer" />
+            </div>
         </div>
     );
 }
@@ -37,7 +45,7 @@ interface NavItemProps {
 
 function NavItem({ href, icon, text, isActive }: NavItemProps) {
     return (
-        <li className={`font-bold flex items-center pl-4 py-2 text-base bg-action cursor-pointer ${isActive ? 'text-white bg-primary' : 'text-primary'}`}>
+        <li className={`font-bold flex items-center pl-4 py-2 text-base cursor-pointer ${isActive ? 'text-white bg-primary' : 'text-primary'}`}>
             <Link href={href}>
                 <button className="flex items-center w-full justify-between">
                     <div className="flex items-center">
