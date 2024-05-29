dcbbbbee05a61c36070a7232f11d81a85e638ba3
Eduardo Aguilar <Agu1laredu@hotmail.com>
2024-05-28 21:17:30 -0300

    fix code

--- src/app/gestionusuarios/components/Table/UserTable.tsx
+++ src/app/gestionusuarios/components/Table/UserTable.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+'use client'
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
-import Filter from '../../../../../assets/Filter.png'
-import Search from '../../../../../assets/Search.png'
+import Filter from '../../../../../assets/Filter.png';
+import Search from '../../../../../assets/Search.png';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import { AiOutlineEdit, AiOutlineDelete } from 'react-icons/ai';
 
 interface User {
   name: string;
@@ -11,76 +15,92 @@ interface User {
   status: string;
 }
 
-const users: User[] = [
-  { name: 'Hanna Baker', email: 'hanna@company.com', id: '123456789', program: 'Diseño', status: 'Activo' },
-  { name: 'Hanna Baker', email: 'hanna@company.com', id: '123456789', program: 'Economía', status: 'Graduado' },
-  { name: 'Hanna Baker', email: 'hanna@company.com', id: '123456789', program: 'Contaduría', status: 'Inactivo' },
-  { name: 'Hanna Baker', email: 'hanna@company.com', id: '123456789', program: 'Ing. civil', status: 'Activo' },
-  { name: 'Hanna Baker', email: 'hanna@company.com', id: '123456789', program: 'Ing. industrial', status: 'Graduado' },
-  { name: 'Hanna Baker', email: 'hanna@company.com', id: '123456789', program: 'Publicidad', status: 'Inactivo' },
-  { name: 'Hanna Baker', email: 'hanna@company.com', id: '123456789', program: 'Ing. civil', status: 'Activo' }
-];
-
 const UserTable: React.FC = () => {
+
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    const fetchedUser = [
+      { name: 'Hanna Baker', email: 'hanna@company.com', id: '123456789', program: 'Diseño', status: 'Activo' },
+      { name: 'Hanna Baker', email: 'hanna@company.com', id: '123456789', program: 'Economía', status: 'Graduado' },
+      { name: 'Hanna Baker', email: 'hanna@company.com', id: '123456789', program: 'Contaduría', status: 'Inactivo' },
+      { name: 'Hanna Baker', email: 'hanna@company.com', id: '123456789', program: 'Ing. civil', status: 'Activo' },
+      { name: 'Hanna Baker', email: 'hanna@company.com', id: '123456789', program: 'Ing. industrial', status: 'Graduado' },
+      { name: 'Hanna Baker', email: 'hanna@company.com', id: '123456789', program: 'Publicidad', status: 'Inactivo' },
+      { name: 'Hanna Baker', email: 'hanna@company.com', id: '123456789', program: 'Ing. civil', status: 'Activo' }
+    ];
+    setUsers(fetchedUser);
+  }, []);
+
+  const statusBodyTemplate = (rowData: User) => {
+    let statusClass = '';
+
+    switch (rowData.status) {
+      case 'Activo':
+        statusClass = 'bg-green-100 text-green-800';
+        break;
+      case 'Graduado':
+        statusClass = 'bg-blue-100 text-blue-800';
+        break;
+      case 'Inactivo':
+        statusClass = 'bg-red-100 text-red-800';
+        break;
+      default:
+        statusClass = 'bg-gray-100 text-gray-800';
+        break;
+    }
+
+    return (
+      <span className={`p-2 border-round ${statusClass}`}>
+        {rowData.status}
+      </span>
+    );
+  };
+
+  const actionBodyTemplate = () => {
+    return (
+      <div className="flex justify-around">
+        <button className="text-blue-500"><AiOutlineEdit size={20} /></button>
+        <button className="text-red-500"><AiOutlineDelete size={20} /></button>
+      </div>
+    );
+  };
+
   return (
     <div className="flex-1 p-6 bg-white rounded-lg shadow m-4">
       <h1 className='font-bold text-blue-500 text-2xl mb-5'>Lista de estudiantes</h1>
       <div className="flex justify-between mb-4">
-      <div className="relative w-full sm:w-1/2">
-      <input
-        type="text"
-        placeholder="Buscar estudiante"
-        className="w-full p-2 pl-10 border rounded"
-      />
-      <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-        <Image src={Search} alt="Buscar" width={20} height={20} />
-      </div>
-    </div>
+        <div className="relative w-full sm:w-1/2">
+          <input
+            type="text"
+            placeholder="Buscar estudiante"
+            className="w-full p-2 pl-10 border rounded"
+          />
+          <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <Image src={Search} alt="Buscar" width={20} height={20} />
+          </div>
+        </div>
         <div className="flex space-x-2">
-          <Image 
-            src={Filter} 
-            alt="Filtro" 
+          <Image
+            src={Filter}
+            alt="Filtro"
             className='mr-4 cursor-pointer'
-            width={24} 
-            height={24} 
+            width={24}
+            height={24}
           />
           <button className="bg-blue-200 text-blue-500 font-semibold py-2 px-4 rounded">Crear nuevo estudiante</button>
-          <button className="bg-blue-500 text-white  py-2 px-4 rounded">Carga masiva</button>
+          <button className="bg-blue-500 text-white py-2 px-4 rounded">Carga masiva</button>
         </div>
       </div>
 
-
-      <table className="w-full border-collapse">
-        <thead className="bg-blue-200 rounded-full text-blue-500">
-          <tr>
-            <th className="p-2">Nombre</th>
-            <th className="p-2">Correo</th>
-            <th className="p-2">ID Estudiante</th>
-            <th className="p-2">Programa</th>
-            <th className="p-2">Estado académico</th>
-            <th className="p-2">Acciones</th>
-          </tr>
-        </thead>
-        <tbody className='text-center'>
-          {users.map((user, index) => (
-            <tr key={index} className="bg-white">
-              <td className="p-2">{user.name}</td>
-              <td className="p-2">{user.email}</td>
-              <td className="p-2">{user.id}</td>
-              <td className="p-2">{user.program}</td>
-              <td className="p-2">
-                <span className={`p-1 rounded ${user.status === 'Activo' ? 'bg-green-200' : user.status === 'Graduado' ? 'bg-yellow-200' : 'bg-red-200'}`}>
-                  {user.status}
-                </span>
-              </td>
-              <td className="p-2 flex justify-around">
-                <button className="text-blue-500">✏️</button>
-                <button className="text-red-500">🗑️</button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <DataTable value={users} tableStyle={{ minWidth: '50rem' }} className="custom-table">
+        <Column field="name" header="Nombre" headerStyle={{ fontWeight: 'bold', fontSize: '1.2rem', color: '#000' }}></Column>
+        <Column field="email" header="Correo" headerStyle={{ fontWeight: 'bold', fontSize: '1.2rem', color: '#000' }}></Column>
+        <Column field="id" header="ID Estudiante" headerStyle={{ fontWeight: 'bold', fontSize: '1.2rem', color: '#000' }}></Column>
+        <Column field="program" header="Programa" headerStyle={{ fontWeight: 'bold', fontSize: '1.2rem', color: '#000' }}></Column>
+        <Column field="status" header="Estado Académico" body={statusBodyTemplate} headerStyle={{ fontWeight: 'bold', fontSize: '1.2rem', color: '#000' }}></Column>
+        <Column body={actionBodyTemplate} headerStyle={{ fontWeight: 'bold', fontSize: '1.2rem', color: '#000' }}></Column>
+      </DataTable>
     </div>
   );
 };
